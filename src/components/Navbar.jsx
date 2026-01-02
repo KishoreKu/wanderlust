04b92c8533d67dcb3a5d2e87e46cc51933896f91
KishoreKu <akishorecert@gmail.com>
2026-01-02 01:15:43 -0500

    Update navbar with transparent PNG Gubbu logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ export function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <Link to="/" className="flex items-center">
-            <img src="/gubbu-complete-logo.jpg" alt="Gubbu - Navigate the Modern World" className="h-16 w-auto object-contain" />
+            <img src="/gubbu-logo.png" alt="Gubbu - Navigate the Modern World" className="h-16 w-auto object-contain" />
           </Link>
 
           <div className="hidden md:flex items-center space-x-8">
